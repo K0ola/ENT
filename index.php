a2e513cbe040883828d102055a20def4f06fc76f
K0la <124051933+K0ola@users.noreply.github.com>
2023-12-19 13:52:27 +0100

    :rocket:
    ajout des bases pour la connxion

--- index.php
+++ index.php
@@ -4,10 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./src/styles/themes.css">
+    <link rel="stylesheet" href="./src/styles/index.css">
+    <link rel="stylesheet" href="./src/styles/root.css">
     <title>Document</title>
 </head>
+
+<?php
+    include('./src/scripts/connexion_bdd.php');
+    $requete="SELECT * FROM utilisateurs";
+    $stmt=$db->query($requete);
+    $result=$stmt->fetchall(PDO::FETCH_ASSOC);
+    
+?>
 <body>
-    <div id="themeOptions">
+    <?php
+    if (isset($_SESSION["login"])){
+        require("./src/pages/panel.php");
+    } else {
+        include ('./src/pages/connexion.php');  
+    }
+
+    ?>
+
+
+
+    <!-- <div id="themeOptions">
         <button onclick="setTheme('blue')">Bleu</button>
         <button onclick="setTheme('green')">Vert</button>
         <button onclick="setTheme('red')">Rouge</button>
@@ -16,6 +37,6 @@
     
     <h1 id="title">Thème Changer</h1>
 
-    <script src="./src/scripts/themes.js"></script>
+    <script src="./src/scripts/themes.js"></script> -->
 </body>
 </html>
