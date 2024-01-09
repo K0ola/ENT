ac5f3b9df9cab309267e12da00a2df24b4cb7b14
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-09 22:58:31 +0100

    ✨ thème

--- src/pages/dashboard/dashboard_view.php
+++ src/pages/dashboard/dashboard_view.php
@@ -2,11 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Tableau de bord</title>
     <link rel="stylesheet" href="src/root.css">
+    <link rel="stylesheet" href="src/layout/styles/theme.css">
     <link rel="stylesheet" href="src/pages/dashboard/styles/dashboard.css">
 </head>
-<body>
+<body class="Tropical-Blue">
     <div class="dashboard-container">
         
         <?php
