e63279fbb9005f073b1ab1d908d5fb4d0a21434f
Pauline <124241876+Pegazel@users.noreply.github.com>
2024-01-09 17:12:27 +0100

    actualité

---
+++ src/pages/actualite/actualite_controller.php
@@ -0,0 +1,2 @@
+<?php 
+require_once('actualite_view.php');
---
+++ src/pages/actualite/actualite_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="actualite.css">
+    <title>Actualité</title>
+</head>
+<body>
+    <?php require_once('src/layout/nav.php'); ?>
+    <h1>Vos news de l’université</h1>
+</body>
+</html>
