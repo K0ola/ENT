c6834bfd8703e38e9c73ab70b0c9a20b946475ce
Pauline <124241876+Pegazel@users.noreply.github.com>
2024-01-09 11:51:51 +0100

    style connexion

--- src/pages/connexion/connexion_view.php
+++ src/pages/connexion/connexion_view.php
@@ -3,6 +3,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link rel="stylesheet" href="src/root.css">
     <link rel="stylesheet" href="src/layout/styles/theme.css">
