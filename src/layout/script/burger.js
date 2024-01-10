1c6c1a58d28a45d62fd265d824fab0abd7f360a9
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-10 11:09:29 +0100

    📱 ✨ Burger Menu

--- src/layout/script/burger.js
+++ src/layout/script/burger.js
@@ -6,9 +6,11 @@ let flag = 0;
 
 IconBurg.addEventListener("click", () => {
     if(flag == 0) {
+        IconBurg.src = "src/assets/icon/X.svg"
         Nav.classList.add("ouvert");
         flag = 1;
     } else {
+        IconBurg.src = "src/assets/icon/Menu.svg"
         Nav.classList.remove("ouvert");
         flag = 0;
     }
