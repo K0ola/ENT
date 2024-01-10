489667514276220144e13881e4e7e7788d49e2cd
Pauline <124241876+Pegazel@users.noreply.github.com>
2024-01-10 01:08:21 +0100

    all

--- src/layout/nav.php
+++ src/layout/nav.php
@@ -1,31 +1,33 @@
-<?php session_start() ?>
+<?php session_start()?>
 
-<link rel="stylesheet" href="src/layout/styles/header.css">
-<nav>
+<link rel="stylesheet" href="src/layout/styles/nav.css">
+<link rel="stylesheet" href="theme.css">
+<nav class="txt_nav">
     <header>
-        <a href="/notifications"><img src="" alt="Notifications"></a>
-        <a href="/profil"><img src="" alt="Mon Compte"></a>
+        <!-- <a href="/notifications"><img src="" alt="Notifications"></a> -->
+        <a href="/profil" class="Nav_profil noDeco"><img class="Icon_Larger" src="src/assets/user/user_icon.png" alt="Mon Compte" title="Mon Compte"><p><?= $_SESSION['prenom'] ?> <?= $_SESSION['nom'] ?></p></a>
         <a href="src/logout.php"><img src="" alt="Déconnexion"></a>
     </header>
+    <hr/>
     <section>
         <?php
         if ($_SESSION['role'] === 'admin') {
             echo "
-            <a href=\"/dashboard\">Accueil</a>
-            <a href=\"/gestion-comptes\">Gestion utilisateurs</a>
-            <a href=\"/gestion-classes\">Gestion classes</a>
-            <a href=\"/gestion-matieres\">Gestion matières</a>
+            <a href=\"/dashboard\" class=\"noDeco\">Accueil</a>
+            <a href=\"/gestion-comptes\" class=\"noDeco\">Gestion utilisateurs</a>
+            <a href=\"/gestion-classes\" class=\"noDeco\">Gestion classes</a>
+            <a href=\"/gestion-matieres\" class=\"noDeco\">Gestion matières</a>
             ";
         } else if ($_SESSION['role'] === 'prof') {
         } else if ($_SESSION['role'] === 'student') {
             echo "
-            <a href=\"/dashboard\">Accueil</a>
-            <a href=\"/agenda\">Agenda</a>
-            <a href=\"/discussion\">Communication</a>
-            <a href=\"/vie-scolaire\">Vie scolaire</a>
-            <a href=\"/outils\">Outils</a>
-            <a href=\"/cours\">Cours</a>
-            <a href=\"/actualité\">Actualité</a>
+            <a href=\"/dashboard\" class=\"noDeco\">Accueil</a>
+            <a href=\"/agenda\" class=\"noDeco\">Agenda</a>
+            <a href=\"/discussion\" class=\"noDeco\">Communication</a>
+            <a href=\"/vie-scolaire\" class=\"noDeco\">Vie scolaire</a>
+            <a href=\"/outils\" class=\"noDeco\">Outils</a>
+            <a href=\"/cours\" class=\"noDeco\">Cours</a>
+            <a href=\"/actualite\" class=\"noDeco\">Actualité</a>
             ";
         }
         ?>
@@ -33,7 +35,7 @@
 
     <footer>
         <a href="/parametres">Param</a>
-        <a href="/mentions-légales">Mentions légales</a>
+        <a href="/mentions-légales" class="txt_micro">Mentions légales</a>
     </footer>
 
 </nav>
--- src/pages/actualite/actualite_view.php
+++ src/pages/actualite/actualite_view.php
@@ -3,11 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="actualite.css">
+    <link rel="stylesheet" href="src/pages/actualite/actualite.css">
+    <link rel="stylesheet" href="src/root.css">
+    <link rel="stylesheet" href="src/layout/styles/theme.css">
     <title>Actualité</title>
 </head>
 <body>
     <?php require_once('src/layout/nav.php'); ?>
     <h1>Vos news de l’université</h1>
+    <div class="news">
+        <div class="news-1">
+            <img src="src/assets/image-news/serviette.jpg" alt="">
+            <h2>Précarité menstruelle : Le Saviez vous ?</h2>
+            <p>30% des femmes en France ont été confrontées à la précarité menstruelle* Distributeurs de protections périodiques dans tous vos bâtiments de cours. <br>
+            L'équipe de la Mission Egalité souhaite vous faire connaître l'existence de distributeurs de protections périodiques dans tous les bâtiments de l'université. Retrouvez leurs emplacements précis en pièces jointes de cette actualité.
+            <br>
+            Les distributeurs sont régulièrement réapprovisionnés en serviettes et tampons afin que chacun·e d'entre vous puisse avoir accès librement à des protections périodiques à l'université. Si vous constatez qu'il manque des protections périodiques dans un des distributeurs vous pouvez nous envoyer un mail pour nous en informer à l'adresse suivante : mission.egalite@univ-eiffel.fr.
+            <br>
+            * (source : enquête précarité menstruelle, Règles Elémentaires X OpinionWay, Mars 2023)</p>
+            <a href="src/assets/image-news/Emplacement_IFIS.pdf">Emplacement_IFIS.pdf</a>
+            <a href="src/assets/image-news/Emplacement_Marne-la-Vallee.pdf">Emplacement_Marne-la-Vallee.pdf</a>
+            <br><br>
+            <hr>
+            <br>
+            <P>Publié par Leonie Clarac-Dutillieux
+                <br>le 09/01/2024
+            </P>
+        </div>
+        <div class="news-1">
+            <img src="src/assets/image-news/Bandeau.png" alt="">
+            <h2>Conférence : Quelles poursuites d'études après mon Bac+3 ?</h2>
+            <p>Vous êtes en 3e année à l'Université et vous vous interrogez sur votre poursuite d'études ? Participez à notre conférence !
+            <br>
+            Nos conseillers vous aideront à explorer les différentes voies, à choisir un Master et à préparer votre candidature. Ils répondront également à vos questions. 
+            <br>
+            Participez à la visioconférence : <br>
+            Jeudi 21 décembre 2023, à 18h <br>
+            <a href="https://univ-eiffel.zoom.us/j/89837832006">https://univ-eiffel.zoom.us/j/89837832006</a><br>
+            Mot de passe : master2024</p>
+            <a href="src/assets/image-news/Conference_poursuites_bac_3_Affiche_2023.pdf">Conference_poursuites_bac_3_Affiche_2023.pdf</a>
+            <br><br>
+            <hr>
+            <br>
+            <P>Publié par Marion Chevillard
+                <br>le 21/12/2023
+            </P>
+        </div>
+        <div class="news-1">
+            <img src="src/assets/image-news/Affiche.png" alt="">
+            <h2>Retour sur Rentr'Eiffel</h2>
+            <p>Événement d'intégration des étudiant-es <br>
+            Rentr'Eiffel raisonne encore sur le parvis de la Maison de l'Étudiant non?!
+                <br>
+            Retrouvez un résumé des ces trois journées d'intégration (<a href="https://clap.univ-eiffel.fr/videos/rentreiffel/">en cliquant ici</a>) qui ont permis de nous réunir et faire vivre la vie de campus à l'Université Gustave Eiffel!
+                <br>
+            On vous donne RDV en octobre 2024 pour la prochaine édition.
+            </p>
+            <br><br>
+            <hr>
+            <br>
+            <P>Publié par Service de la Vie Étudiante
+                <br>le 15/12/2023
+            </P>
+        </div>
+        <iframe id="insta" title="instagram du BDE de l'université Gustave Eiffel"
+        src="https://www.instagram.com/bde_eiffel/">
+        </iframe>
+    </div>
 </body>
 </html>
