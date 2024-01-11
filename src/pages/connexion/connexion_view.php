f10fc670c934f4814105cac72869dd277e08ac25
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-11 01:26:41 +0100

    💄 New UI connexion

--- src/pages/connexion/connexion_view.php
+++ src/pages/connexion/connexion_view.php
@@ -11,31 +11,36 @@
     <link rel="stylesheet" href="src/pages/connexion/styles/connexion.css">
 </head>
 <body class="Tropical-Blue">
+    <section id="background">
+
+    </section>
     <section id="connexion">
         <header>
             <img src="src/assets/Logo_Universite.svg" alt="Logo de l'université"/>
             <h1 class="txt_h1">ENT MMI - Champs sur Marne</h1>
+            <p>Etudiants, enseignants, intervenants, personnel administratif, bienvenue sur votre environnement numérique de travail MMI !</p>
             <!-- <h1 class="txt_h1">Connexion</h1> -->
         </header>
-        <article>
-            <p>Etudiants, enseignants, intervenants, personnel administratif, bienvenu sur votre enironnement numérique de travail MMI !</p>
-            <form action="/connexion" method="POST">
-                <div class="div_form">
-                    <h2 class="txt_h2">Connexion</h2>
-                    <div>
-                        <label for="login" class="txt_bold">Identifiant</label>
-                        <input type="text" class="input" name="login" id="login" required>
-                    </div>
-                    <div id='mdp'>
-                        <label for="password" class="txt_bold">Mot de passe</label>
-                        <input type="password" class="input" name="password" id="password" required>
-                        <img src="src/assets/icon/Eye-off.svg" class="Icon_Medium" alt="Toggle Visibility" onclick="psswrdvisible()">
-                    </div>
-                    <a href="/mdp-forget" class="txt_micro">Mot de passe oublié ?</a>
+        
+        <form action="/connexion" method="POST">
+            <div class="div_form">
+                <div>
+                    <label for="login" class="txt_bold">Identifiant</label>
+                    <input type="text" class="input" name="login" id="login" placeholder="prenom.nom" required>
                 </div>
+                <div id='mdp'>
+                    <label for="password" class="txt_bold">Mot de passe</label>
+                    <input type="password" class="input" name="password" id="password" placeholder="*************"  required>
+                    <img src="src/assets/icon/Eye-off.svg" class="Icon_Medium" alt="Toggle Visibility" onclick="psswrdvisible()">
+                </div>
+                <a href="/mdp-forget" class="txt_micro">Mot de passe oublié ?</a>
+            </div>
+            <label class="connexionBTN_container txt_nav">
                 <input type="submit" value="Connexion" id="btn_connexion">
-            </form>
-        </article>
+                Connexion
+                <div class="Icon_Large"></div>
+            </label>
+        </form>
     </section>
     <script src="src/layout/script/theme.js"></script>
     <script src="src/pages/connexion/script.js"></script>
